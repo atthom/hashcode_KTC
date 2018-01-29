cf0af735203d73d79e3dffaf3fe7476ed4377c4e
Thomas Jalabert <thom.jalabert@gmail.com>
2018-01-29 15:04:00 +0100

    submit script is working

--- pizza.py
+++ pizza.py
@@ -2,12 +2,27 @@ import os
 import sys
 
 
-def answer(path):
-    return 0
+class Slice:
+    def __init__(self, corner1, corner2):
+        self.corner1 = corner1
+        self.corner2 = corner2
+
+    def __repr__(self):
+        return " ".join([str(self.corner1[0]), str(self.corner2[0]), str(self.corner1[1]), str(self.corner2[1])])
 
 
+def get_answer(path):
+    li = []
+    li.append(Slice((0, 2), (0, 1)))
+    li.append(Slice((0, 2), (2, 2)))
+    li.append(Slice((0, 2), (3, 4)))
+    answer = str(len(li)) + "\r"
+    for s in li:
+        answer += str(s) + "\r"
+
+    return answer
 
 
 if __name__ == '__main__':
-    arg1 = sys.argv[0]
-    print(answer(arg1))
+    arg1 = sys.argv[-1]
+    print(get_answer(arg1))
