57d02ee81295add979ca0d8eb1a2f871e9f72c1c
Thomas Jalabert <thom.jalabert@gmail.com>
2018-03-01 19:28:54 +0100

    add datastrucutre

--- googlecar.py
+++ googlecar.py
@@ -1,6 +1,39 @@
 from utils import *
 
 
+class Coordinates:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def distance(self, c):
+        pass
+
+
+class Vehicles:
+    def __init__(self):
+        self.position = Coordinates(0, 0)
+
+    def TotalTimeToRide(ride):
+        pass
+
+
+class Ride:
+    def __init__(self, depart, arrive, start, finish):
+        self.depart = depart
+        self.arrive = arrive
+        self.start = start
+        self.finish = finish
+
+
+class Solver:
+    def __init__(self, shape, nb_steps, vehicles, rides):
+        self.shape = shape
+        self.vehicles = vehicles
+        self.nb_steps = nb_steps
+        self.rides = rides
+
+
 def parser():
     pass
 
