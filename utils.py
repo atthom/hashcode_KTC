8b20ae7a2bcf0c57f9576a98e3a4da224cf5896a
Thomas Jalabert <thom.jalabert@gmail.com>
2018-03-01 18:57:30 +0100

    add example

--- utils.py
+++ utils.py
@@ -9,14 +9,3 @@ def knap(size, weight, capacity):
     knapsack.knapsack(size, weight).solve(capacity)
     """
     return knapsack.knapsack(size, weight).solve(capacity)
-
-
-size = [21, 11, 15, 9, 34, 25, 41, 52]
-weight = [22, 12, 16, 10, 35, 26, 42, 53]
-capacity = 150
-ll = knapsack.knapsack(size, weight).solve(capacity)
-print(ll)
-dd = 0
-for i in ll[1]:
-    dd += (weight[i])
-print(dd)
