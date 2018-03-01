769b7e03428dfd98edebcad4b111210e3ec942d7
GNINGK <khadim.gning@yahoo.fr>
2018-03-01 20:06:50 +0100

    parser

--- googlecar.py
+++ googlecar.py
@@ -1,4 +1,4 @@
-from utils import *
+import sys
 
 
 class Coordinates:
@@ -34,8 +34,19 @@ class Solver:
         self.rides = rides
 
 
-def parser():
-    pass
+def parser(filename):
+    my_file = open(filename)
+    lines = my_file.readlines()
+    sim_info = lines[0].split()
+    i = 1
+    rides_nb = len(lines) - 1
+    rides = [None] * rides_nb
+    while i < rides_nb + 1:
+        ride_info = lines[i]
+        rides[i-1] = Ride(Coordinates(ride_info[0], ride_info[1]), Coordinates(ride_info[2], ride_info[3]), ride_info[4], ride_info[5])
+        i += 1
+
+    Solver(Coordinates(sim_info[0], sim_info[1]), sim_info[5], [Vehicles]*int(sim_info[2]), rides)
 
 
 def solver():
@@ -47,4 +58,5 @@ def answer():
 
 
 if __name__ == '__main__':
-    answer()
+    # answer()
+    parser(sys.argv[1])
