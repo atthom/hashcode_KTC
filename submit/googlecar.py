307da9b0468ddf3e7d319086c7882e55f7ab1a29
GNINGK <khadim.gning@yahoo.fr>
2018-03-01 20:07:09 +0100

    parser

--- submit/googlecar.py
+++ submit/googlecar.py
@@ -1,8 +1,54 @@
-from utils import *
+import sys
 
 
-def parser():
-    pass
+class Coordinates:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def distance(self, c):
+        pass
+
+
+class Vehicles:
+    def __init__(self):
+        self.position = Coordinates(0, 0)
+
+    def TotalTimeToRide(ride):
+        pass
+
+
+class Ride:
+    def __init__(self, depart, arrive, start, finish):
+        self.depart = depart
+        self.arrive = arrive
+        self.start = start
+        self.finish = finish
+
+
+class Solver:
+    def __init__(self, shape, nb_steps, vehicles, rides):
+        self.shape = shape
+        self.vehicles = vehicles
+        self.nb_steps = nb_steps
+        self.rides = rides
+
+
+def parser(filename):
+    my_file = open(filename)
+    lines = my_file.readlines()
+    sim_info = lines[0].split()
+    # 3    rows, 4    columns, 2    vehicles, 3    rides, 2   bonus and 10 steps
+    i = 1
+    rides_nb = len(lines) - 1
+    print 'len' + str(rides_nb)
+    rides = [None] * rides_nb
+    while i < rides_nb + 1:
+        ride_info = lines[i]
+        print 'info' + ride_info
+        rides[i-1] = Ride(Coordinates(ride_info[0], ride_info[1]), Coordinates(ride_info[2], ride_info[3]), ride_info[4], ride_info[5])
+        i += 1
+    Solver(Coordinates(sim_info[0], sim_info[1]), sim_info[5], [Vehicles]*int(sim_info[2]), rides)
 
 
 def solver():
@@ -14,4 +60,5 @@ def answer():
 
 
 if __name__ == '__main__':
-    answer()
+    # answer()
+    parser(sys.argv[1])
