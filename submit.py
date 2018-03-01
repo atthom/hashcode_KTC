143285416d6afa603ff827f781ec973af389ee64
Thomas Jalabert <thom.jalabert@gmail.com>
2018-03-01 18:09:45 +0100

    reset

--- submit.py
+++ submit.py
@@ -1,25 +1,21 @@
-import os
 import shutil
-from os import listdir
+from os import listdir, mkdir
 from os.path import isfile, join
 from subprocess import check_output
 
-
 input_files = [f for f in listdir(".") if isfile(join(".", f)) and ".in" in f]
-
 input_files = ['example.in', 'small.in']
 
 for file in input_files:
     with open(file.replace(".in", ".out"), "w+") as f:
         print(file, "...")
-        f.write(check_output(["python", "pizza.py", file]).decode("utf-8"))
-
+        f.write(check_output(
+            ["python", "pizza_answer_example.py", file]).decode("utf-8"))
 
 if os.path.exists("./submit"):
     shutil.rmtree("./submit")
 
-os.mkdir("./submit")
-shutil.copyfile("pizza.py", "./submit/pizza.py")
+mkdir("./submit")
+shutil.copyfile("pizza_answer_example.py", "./submit/pizza.py")
 shutil.copyfile("submit.py", "./submit/submit.py")
-
 shutil.make_archive("submit", 'zip', "./submit")
